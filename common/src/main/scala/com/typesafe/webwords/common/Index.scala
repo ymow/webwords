066f1924fed37343cd0a70632c4025f7a06c58ad
Havoc Pennington <hp@pobox.com>
2011-09-19 14:51:12 -0400

    add toString to Index class

--- common/src/main/scala/com/typesafe/webwords/common/Index.scala
+++ common/src/main/scala/com/typesafe/webwords/common/Index.scala
@@ -4,5 +4,9 @@ import java.net.URL
 
 class Index(val url: URL,
     val links: Seq[(String, String)],
-    val wordCounts: Seq[(String, Int)])
+    val wordCounts: Seq[(String, Int)]) {
+    override def toString = {
+        "Index(" + url + "," + links.size + " links," + wordCounts.size + " counts)"
+    }
+}
 
