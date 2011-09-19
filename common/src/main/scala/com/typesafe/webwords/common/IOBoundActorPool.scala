4b3e55b434e43f3d58981d736c2fc927ea79cb3e
Havoc Pennington <hp@pobox.com>
2011-09-19 13:52:01 -0400

    add IOBoundActorPool and tweak CPUBoundActorPool a bit

--- common/src/main/scala/com/typesafe/webwords/common/IOBoundActorPool.scala
+++ common/src/main/scala/com/typesafe/webwords/common/IOBoundActorPool.scala
@@ -5,38 +5,30 @@ import akka.routing._
 
 /**
  * An actor pool with a bunch of reasonable defaults for actors
- * that do some kind of CPU-bound computation. General loose
- * assumptions are that having way more actors than cores is
- * counterproductive, that we only want to process each message
- * one time, and that the actors in the pool are relatively
- * lightweight.
+ * that do some kind of blocking IO-bound work. General loose
+ * assumptions are that the requests to the actors are round-trips with
+ * replies (active futures), the actors won't use much CPU, that we only want
+ * to process each message one time, and that the actors in the pool could
+ * be expensive to create (e.g. establishing a network connection).
  *
  * A lot of the values set here are pretty arbitrary and could only
  * be set scientifically with a lot of application-specific tuning.
  */
-trait CPUBoundActorPool
+trait IOBoundActorPool
     extends DefaultActorPool
-    with SmallestMailboxSelector
+    with ActiveFuturesPressureCapacitor
     with BoundedCapacityStrategy
-    with MailboxPressureCapacitor
     with Filter
     with BasicRampup
     with BasicBackoff {
     self: Actor =>
 
-    // Selector: selectionCount is how many pool members to send each message to
-    override def selectionCount = 1
-    // Selector: partialFill controls whether to pick less than selectionCount or
-    // send the same message to duplicate delegates, when the pool is smaller
-    // than selectionCount. Does not matter if lowerBound >= selectionCount.
-    override def partialFill = true
     // BoundedCapacitor: create between lowerBound and upperBound delegates in the pool
     override val lowerBound = 1
-    override lazy val upperBound = Runtime.getRuntime().availableProcessors() * 2
-    // MailboxPressureCapacitor: pressure is number of delegates with >pressureThreshold messages queued
-    override val pressureThreshold = 1
+    override lazy val upperBound = 50
     // BasicRampup: rampupRate is percentage increase in capacity when all delegates are busy
-    override def rampupRate = 0.2
+    // make this very small to aim for just 1 more actor per needed connection
+    override def rampupRate = 0.05
     // BasicBackoff: backoffThreshold is the percentage-busy to drop below before
     // we reduce actor count
     override def backoffThreshold = 0.7
