94ea504cc91e342e6eee4ca3405f09225a282c9e
Havoc Pennington <hp@pobox.com>
2011-09-20 09:40:16 -0400

    Implement a SpiderActor to shallow-spider a URL and compute some stuff

---
+++ indexer/src/main/scala/com/typesafe/webwords/indexer/SpiderActor.scala
@@ -0,0 +1,166 @@
+package com.typesafe.webwords.indexer
+
+import scala.collection.generic.CanBuildFrom
+import scala.collection.mutable.Builder
+import akka.actor.{ Index => _, _ }
+import akka.dispatch._
+import java.net.URL
+import com.typesafe.webwords.common._
+import java.net.URI
+import java.io.File
+import java.util.concurrent.CountDownLatch
+
+sealed trait SpiderRequest
+case class Spider(url: URL) extends SpiderRequest
+
+sealed trait SpiderReply
+case class Spidered(url: URL, index: Index)
+
+/**
+ * This actor coordinates requests to the other actors to
+ * eventually put together a shallow spider of a URL.
+ */
+class SpiderActor
+    extends Actor {
+    private val indexer = Actor.actorOf(new IndexerActor)
+    private val fetcher = Actor.actorOf(new URLFetcher)
+
+    override def preStart() = {
+        indexer.start
+        fetcher.start
+    }
+
+    override def postStop() = {
+        indexer.stop
+        fetcher.stop
+    }
+
+    override def receive = {
+        case request: SpiderRequest => request match {
+            case Spider(url) =>
+                self.channel.replyWith(SpiderActor.spider(indexer, fetcher, url))
+        }
+    }
+}
+
+object SpiderActor {
+    private def fetchBody(fetcher: ActorRef, url: URL): Future[String] = {
+        val fetched = fetcher ? FetchURL(url)
+        fetched map { result =>
+            result match {
+                case URLFetched(status, headers, body) if status == 200 =>
+                    body
+                case URLFetched(status, headers, body) =>
+                    throw new Exception("Failed to fetch, status: " + status)
+                case _ =>
+                    throw new IllegalStateException("Unexpected reply to url fetch: " + result)
+            }
+        }
+    }
+
+    private def fetchIndex(indexer: ActorRef, fetcher: ActorRef, url: URL): Future[Index] = {
+        fetchBody(fetcher, url) flatMap { body =>
+            val indexed = indexer ? IndexHtml(url, body)
+            indexed map { result =>
+                result match {
+                    case IndexedHtml(index) =>
+                        index
+                }
+            }
+        }
+    }
+
+    // pick a few links on the page to follow, preferring to "descend"
+    private def childLinksToFollow(url: URL, index: Index): Seq[URL] = {
+        val uri = removeFragment((url.toURI))
+        val siteRoot = copyURI(uri, path = Some(null))
+        val parentPath = new File(uri.getPath).getParent
+        val parent = if (parentPath != null) copyURI(uri, path = Some(parentPath)) else siteRoot
+
+        val sameSiteOnly = index.links map {
+            kv => kv._2
+        } map {
+            new URI(_)
+        } map {
+            removeFragment(_)
+        } filter {
+            _ != uri
+        } filter {
+            isBelow(siteRoot, _)
+        } sortBy {
+            pathDepth(_)
+        }
+        val siblingsOrChildren = sameSiteOnly filter { isBelow(parent, _) }
+        val children = siblingsOrChildren filter { isBelow(uri, _) }
+
+        // prefer children, if not enough then siblings, if not enough then same site
+        val toFollow = (children ++ siblingsOrChildren ++ sameSiteOnly).distinct take 10 map { _.toURL }
+        toFollow
+    }
+
+    private def mergeIndexes(a: Index, b: Index): Index = {
+        val links = (a.links ++ b.links).sortBy(_._1).distinct
+        val counts = (a.wordCounts ++ b.wordCounts).sortBy(0 - _._2).take(math.max(a.wordCounts.length, b.wordCounts.length))
+        new Index(links, counts)
+    }
+
+    private def combineIndexes(indexes: TraversableOnce[Index]): Index = {
+        indexes.reduce(mergeIndexes(_, _))
+    }
+
+    /*
+     * This is a relatively complex example of using Akka's Future class.
+     * Note that this code never blocks (no future.await()
+     * or future.get()), instead it uses map, flatMap, and friends
+     * on the futures to chain them together. It then ultimately
+     * returns a future that will be completed when all the dependent
+     * futures are also completed.
+     */
+    private[indexer] def spider(indexer: ActorRef, fetcher: ActorRef, url: URL): Future[Spidered] = {
+        implicit val dispatcher = indexer.dispatcher
+        fetchIndex(indexer, fetcher, url) flatMap { rootIndex =>
+            val childIndexes = childLinksToFollow(url, rootIndex) map { fetchIndex(indexer, fetcher, _) }
+            val rootIndexFuture = new DefaultCompletableFuture[Index] // some 1.2 bug: AlreadyCompletedFuture breaks
+            rootIndexFuture.completeWithResult(rootIndex)
+            val allIndexFutures = childIndexes ++ Iterator(rootIndexFuture)
+            val allIndexes = Future.sequence(allIndexFutures)
+            val combinedIndex = allIndexes map { indexes =>
+                combineIndexes(indexes)
+            }
+            val spidered = combinedIndex map { index =>
+                Spidered(url, index)
+            }
+
+            spidered
+        }
+    }
+
+    // this lets us copy URIs changing one part of the URI via keyword.
+    private def copyURI(uri: URI, scheme: Option[String] = None, userInfo: Option[String] = None,
+        host: Option[String] = None, port: Option[Int] = None, path: Option[String] = None,
+        query: Option[String] = None, fragment: Option[String] = None): URI = {
+        new URI(if (scheme.isEmpty) uri.getScheme else scheme.get,
+            if (userInfo.isEmpty) uri.getUserInfo else userInfo.get,
+            if (host.isEmpty) uri.getHost else host.get,
+            if (port.isEmpty) uri.getPort else port.get,
+            if (path.isEmpty) uri.getPath else path.get,
+            if (query.isEmpty) uri.getQuery else query.get,
+            if (fragment.isEmpty) uri.getFragment else fragment.get)
+    }
+
+    private[indexer] def removeFragment(uri: URI) = {
+        if (uri.getFragment != null)
+            copyURI(uri, fragment = Some(null))
+        else
+            uri
+    }
+
+    private[indexer] def isBelow(uri: URI, possibleChild: URI) = {
+        val r = uri.relativize(possibleChild)
+        !r.isAbsolute && uri != possibleChild
+    }
+
+    private[indexer] def pathDepth(uri: URI) = {
+        uri.getPath.count(_ == '/')
+    }
+}
---
+++ indexer/src/test/scala/com/typesafe/webwords/indexer/SpiderActorSpec.scala
@@ -0,0 +1,116 @@
+package com.typesafe.webwords.indexer
+
+import org.scalatest.matchers._
+import org.scalatest._
+import akka.actor.{ Index => _, _ }
+import java.net.URL
+import com.typesafe.webwords.common._
+import java.net.URI
+
+class SpiderActorSpec extends FlatSpec with ShouldMatchers with BeforeAndAfterAll {
+    var httpServer: TestHttpServer = null
+
+    override def beforeAll = {
+        httpServer = new TestHttpServer(Some(this.getClass))
+        httpServer.start()
+    }
+
+    override def afterAll = {
+        httpServer.stop()
+        httpServer = null
+    }
+
+    behavior of "local http server used to test spider"
+
+    it should "fetch our test resource" in {
+        val fetcher = Actor.actorOf(new URLFetcher).start
+        val f = fetcher ? FetchURL(httpServer.resolve("/resource/Functional_programming.html"))
+        f.get match {
+            case URLFetched(status, headers, body) =>
+                if (status != 200)
+                    println(body)
+                status should be(200)
+            case _ =>
+                throw new Exception("Wrong reply message from fetcher")
+        }
+        fetcher.stop
+    }
+
+    behavior of "utility functions"
+
+    it should "remove a uri fragment" in {
+        val uri = new URI("http://example.com:907/foo?hello=world#bar")
+        uri.getFragment should be("bar")
+        val noFragment = SpiderActor.removeFragment(uri)
+        noFragment.getFragment should be(null)
+
+        noFragment.getScheme should be(uri.getScheme)
+        noFragment.getHost should be(uri.getHost)
+        noFragment.getPort should be(uri.getPort)
+        noFragment.getPath should be(uri.getPath)
+        noFragment.getQuery should be(uri.getQuery)
+        noFragment.getUserInfo should be(uri.getUserInfo)
+    }
+
+    it should "know if uris are below other uris" in {
+        val example = new URI("http://example.com/")
+        val example_a = new URI("http://example.com/a")
+        val example_b = new URI("http://example.com/b")
+        val example_a_c = new URI("http://example.com/a/c")
+        val example_a_d = new URI("http://example.com/a/d")
+        val example_b_e = new URI("http://example.com/b/e")
+        val elsewhere = new URI("http://typesafe.com/")
+        val elsewhere_a = new URI("http://typesafe.com/a")
+
+        val correct = Seq(
+            (example, example) -> false,
+            (example, example_a) -> true,
+            (example_a, example_a_c) -> true,
+            (example_a, example_b) -> false,
+            (example_a, example_b_e) -> false,
+            (example_a_c, example_a_d) -> false,
+            (elsewhere, elsewhere_a) -> true,
+            (elsewhere, example) -> false,
+            (elsewhere, example_a) -> false,
+            (elsewhere_a, example_a) -> false,
+            (elsewhere_a, example_a_c) -> false)
+
+        for (((parent, possibleChild), result) <- correct) {
+            try {
+                SpiderActor.isBelow(parent, possibleChild) should be(result)
+            } catch {
+                case e: Throwable =>
+                    println("parent=" + parent)
+                    println("possibleChild=" + possibleChild)
+                    throw e
+            }
+        }
+    }
+
+    it should "compute uri path depth" in {
+        SpiderActor.pathDepth(new URI("/")) should be(1)
+        SpiderActor.pathDepth(new URI("/a")) should be(1)
+        SpiderActor.pathDepth(new URI("/a/b")) should be(2)
+        SpiderActor.pathDepth(new URI("/a/b/c")) should be(3)
+    }
+
+    behavior of "SpiderActor"
+
+    it should "spider from test http server" in {
+        val url = httpServer.resolve("/resource/ToSpider.html")
+        val spider = Actor.actorOf(new SpiderActor).start
+        val indexFuture = (spider ? Spider(url)) map { result =>
+            result match {
+                case Spidered(url, index) =>
+                    index
+                case _ =>
+                    throw new Exception("Got bad result from Spider: " + result)
+            }
+        }
+        val index = indexFuture.get
+
+        index.wordCounts.size should be(50)
+        val nowheres = (index.links filter { link => link._2.endsWith("/nowhere") } map { _._1 }).sorted
+        nowheres should be(Seq("a", "d", "e", "f", "g", "h", "j", "k", "m", "o"))
+    }
+}
