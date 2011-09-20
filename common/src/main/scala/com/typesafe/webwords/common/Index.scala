fbff963f397e50af80637636f4de3e9a385eb96e
Havoc Pennington <hp@pobox.com>
2011-09-20 16:18:51 -0400

    add equals and hashCode to Index, and make it final

--- common/src/main/scala/com/typesafe/webwords/common/Index.scala
+++ common/src/main/scala/com/typesafe/webwords/common/Index.scala
@@ -1,9 +1,23 @@
 package com.typesafe.webwords.common
 
-class Index(val links: Seq[(String, String)],
+final class Index(val links: Seq[(String, String)],
     val wordCounts: Seq[(String, Int)]) {
     override def toString = {
         "Index(" + links.size + " links," + wordCounts.size + " counts)"
     }
-}
 
+    override def equals(other: Any): Boolean = {
+        other match {
+            case that: Index => {
+                // no canEqual since we're final
+                links == that.links &&
+                    wordCounts == that.wordCounts
+            }
+            case _ => false
+        }
+    }
+
+    override def hashCode: Int = {
+        41 * (41 + links.hashCode) + wordCounts.hashCode
+    }
+}
---
+++ common/src/test/scala/com/typesafe/webwords/common/IndexSpec.scala
@@ -0,0 +1,46 @@
+package com.typesafe.webwords.common
+
+import org.scalatest.matchers._
+import org.scalatest._
+import akka.actor._
+
+class IndexSpec extends FlatSpec with ShouldMatchers {
+    private val sampleIndex = new Index(
+        links = Seq(
+            "dogs" -> "http://dogs.com/",
+            "cats" -> "http://cats.com/"),
+        wordCounts = Seq(
+            "hello" -> 10,
+            "world" -> 5,
+            "quick" -> 4,
+            "brown" -> 3))
+    private val copyOfSampleIndex = new Index(links = sampleIndex.links, wordCounts = sampleIndex.wordCounts)
+    private val sampleIndexDifferentOrder = new Index(
+        links = Seq(
+            "cats" -> "http://cats.com/",
+            "dogs" -> "http://dogs.com/"),
+        wordCounts = Seq(
+            "world" -> 5,
+            "hello" -> 10,
+            "brown" -> 3,
+            "quick" -> 4))
+    private val emptyIndex = new Index(Nil, Nil)
+
+    behavior of "Index"
+
+    it should "have working equals and hashCode" in {
+        sampleIndex should be(sampleIndex)
+        sampleIndex.hashCode should be(sampleIndex.hashCode)
+        sampleIndex should be(copyOfSampleIndex)
+        sampleIndex.hashCode should be(copyOfSampleIndex.hashCode)
+        emptyIndex should be(emptyIndex)
+        emptyIndex.hashCode should be(emptyIndex.hashCode)
+
+        sampleIndex should not be (new Object)
+
+        sampleIndex should not be (sampleIndexDifferentOrder)
+        sampleIndex.hashCode should not be (sampleIndexDifferentOrder.hashCode)
+        sampleIndex should not be (emptyIndex)
+        sampleIndex.hashCode should not be (emptyIndex.hashCode)
+    }
+}
