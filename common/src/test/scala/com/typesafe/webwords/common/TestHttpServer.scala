78222df926ec1efa030a878211847cfdbf852f56
Havoc Pennington <hp@pobox.com>
2011-09-20 10:44:14 -0400

    add /resource/<resourcename> paths on TestHttpServer

--- common/src/test/scala/com/typesafe/webwords/common/TestHttpServer.scala
+++ common/src/test/scala/com/typesafe/webwords/common/TestHttpServer.scala
@@ -8,8 +8,9 @@ import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 import java.net.URI
 import java.util.Random
+import org.eclipse.jetty.util.IO
 
-class TestHttpServer {
+class TestHttpServer(extraResourceClass: Option[Class[_]] = None) {
     private var maybeServer: Option[Server] = None
 
     // this bool is just to detect bugs, it's not thread-safe
@@ -20,6 +21,20 @@ class TestHttpServer {
     private val random = new Random()
     private def randomInRange(min: Int, max: Int) = {
         random.nextInt(max - min + 1) + min
+    }
+
+    private def writeResource(resource: String, response: HttpServletResponse) = {
+        val url = extraResourceClass flatMap { klass =>
+            Option(klass.getResource(resource))
+        } getOrElse {
+            Option(getClass.getResource(resource)) getOrElse {
+                throw new Exception("resource not found: '" + resource + "'")
+            }
+        }
+
+        val in = url.openStream()
+        val out = response.getOutputStream()
+        IO.copy(in, out)
     }
 
     private class TestHandler extends AbstractHandler {
@@ -46,7 +61,30 @@ class TestHttpServer {
                     }
                     jettyRequest.setHandled(true)
                 }
+                case resourcePath: String if resourcePath.startsWith("/resource/") =>
+                    val resource = resourcePath.substring("/resource/".length)
+                    try {
+                        if (resource.endsWith(".html"))
+                            response.setContentType("text/html")
+                        else
+                            throw new Exception("Unable to serve resource with unknown type")
+                        writeResource(resource, response)
+                        response.setStatus(HttpServletResponse.SC_OK)
+                    } catch {
+                        case e: Throwable =>
+                            response.setStatus(HttpServletResponse.SC_NOT_FOUND)
+                            response.setContentType("text/plain")
+                            val writer = response.getWriter
+                            writer.append(e.getClass.getName + ": " + e.getMessage + "\n")
+                            // (if this were an internet-facing web server we might not want this
+                            // since it could leak information, of course)
+                            writer.append(e.getStackTraceString)
+                            writer.append("\n")
+                    }
+                    jettyRequest.setHandled(true)
                 case _ =>
+                    response.setStatus(HttpServletResponse.SC_NOT_FOUND)
+                    jettyRequest.setHandled(true)
             }
         }
     }
