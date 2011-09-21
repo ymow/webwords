9d3823f49fb5eaa638da49d9c03e8b54e705b307
Havoc Pennington <hp@pobox.com>
2011-09-21 16:18:49 -0400

    add a little delay to the start of the web process

--- web/src/main/scala/com/typesafe/webwords/web/WebMain.scala
+++ web/src/main/scala/com/typesafe/webwords/web/WebMain.scala
@@ -3,6 +3,9 @@ package com.typesafe.webwords.web
 import com.typesafe.webwords.common._
 
 object WebMain extends App {
+    // This is a temporary hack to help the web process start later than the worker
+    // and keep AMQP from freaking out.
+    Thread.sleep(5000)
     val server = new WebServer(WebWordsConfig())
     server.start()
     server.run()
