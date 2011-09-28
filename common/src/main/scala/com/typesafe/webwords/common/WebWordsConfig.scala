b4e32bd1ee30faab5c918fe6a870d9cc4530275b
Havoc Pennington <hp@pobox.com>
2011-09-28 09:33:38 -0400

    remove FIXME about println in WebWordsConfig

--- common/src/main/scala/com/typesafe/webwords/common/WebWordsConfig.scala
+++ common/src/main/scala/com/typesafe/webwords/common/WebWordsConfig.scala
@@ -11,7 +11,6 @@ object WebWordsConfig {
         val mongoURL = Option(System.getenv("MONGOHQ_URL"))
         val port = Option(System.getenv("PORT")) map { s => Integer.parseInt(s) }
         val config = WebWordsConfig(amqpURL, mongoURL, port)
-        // FIXME remove this or use a proper logger
         println("Configuration is: " + config)
         config
     }
