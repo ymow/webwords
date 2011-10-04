906a1fff8c626879092a3ffd865c2928b45b8512
Havoc Pennington <hp@pobox.com>
2011-10-04 14:03:13 -0400

    Swallow any and all exceptions from the spidering and caching task
    Need to figure out the real way to get this right with AMQP, but for
    now avoid wedging things up by not sending an exception to AMQP.

--- indexer/src/main/scala/com/typesafe/webwords/indexer/WorkerActor.scala
+++ indexer/src/main/scala/com/typesafe/webwords/indexer/WorkerActor.scala
@@ -3,6 +3,7 @@ package com.typesafe.webwords.indexer
 import akka.actor.{ Index => _, _ }
 import akka.actor.Actor.actorOf
 import akka.dispatch._
+import akka.event.EventHandler
 import com.typesafe.webwords.common._
 import java.net.URL
 
@@ -18,6 +19,13 @@ class WorkerActor(config: WebWordsConfig)
     override def handleRequest(request: WorkQueueRequest): Future[WorkQueueReply] = {
         request match {
             case SpiderAndCache(url) =>
+                // This "neverFailsFuture" is sort of a hacky hotfix; AMQP setup
+                // doesn't react well to returning an exception here, which happens
+                // when there's a bug typically.
+                // We could do various nicer things like send the exception over
+                // the wire cleanly, or configure AMQP differently, but requires
+                // some time to work out. Hotfixing with this.
+                val neverFailsFuture = new DefaultCompletableFuture[WorkQueueReply]
                 val futureIndex = spider ? Spider(new URL(url)) map {
                     _ match { case Spidered(url, index) => index }
                 }
@@ -25,7 +33,15 @@ class WorkerActor(config: WebWordsConfig)
                     cache ? CacheIndex(url, index) map { cacheAck =>
                         SpideredAndCached(url)
                     }
+                } onResult {
+                    case reply: WorkQueueReply =>
+                        neverFailsFuture.completeWithResult(reply)
+                } onException {
+                    case e =>
+                        EventHandler.info(this, "Exception spidering '" + url + "': " + e.getClass.getSimpleName + ": " + e.getMessage)
+                        neverFailsFuture.completeWithResult(SpideredAndCached(url))
                 }
+                neverFailsFuture
         }
     }
 
